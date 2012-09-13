a9819ef667daf028a6d677aea3786b80635a0048
Graham McIntire <gmcintire@gmail.com>
2012-09-13 18:24:36 -0400

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "iain@finalist.nl"
 license           "MIT"
 description       "Installs FreeTDS 0.91"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.1"
+version           "0.0.2"
 
 
 depends "build-essential"
